5087070ea6f74f155ed9332586e43797ac9c5dfa
RobFaiella <robertfaiella@m-ize.com>
2025-04-18 16:10:12 -0400

    Initial Commit

---
+++ A Swift Adventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A Swift Adventure
+//
+//  Created by Rob Faiella on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ A Swift AdventureUITests/A_Swift_AdventureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  A_Swift_AdventureUITestsLaunchTests.swift
+//  A Swift AdventureUITests
+//
+//  Created by Rob Faiella on 4/18/25.
+//
+
+import XCTest
+
+final class A_Swift_AdventureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
