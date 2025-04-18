a7feed9409fb5a87e5652857a70f9202a1671980
RobFaiella <robertfaiella@m-ize.com>
2025-04-18 18:23:45 -0400

    common button

--- A Swift Adventure/ViewControllers/AboutViewController.swift
+++ A Swift Adventure/ViewControllers/AboutViewController.swift
@@ -7,26 +7,12 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, CodeShowable {
+    var codeKey: String { return "AboutViewController" }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Basic setup
         self.view.backgroundColor = .white
-        
-        // Add button to present modal
-        let showCodeButton = UIButton(type: .system)
-        showCodeButton.setTitle("Show Code", for: .normal)
-        showCodeButton.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
-        showCodeButton.addTarget(self, action: #selector(showCode), for: .touchUpInside)
-        
-        self.view.addSubview(showCodeButton)
-    }
-    
-    @objc func showCode() {
-        // Present the modal view
-        let codeViewController = CodeViewController()
-        codeViewController.fileKey = "AboutViewController"
-        self.present(codeViewController, animated: true, completion: nil)
     }
 }
